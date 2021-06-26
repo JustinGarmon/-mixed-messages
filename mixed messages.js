9d836c30067373f01f6427c715b49e38ac7826c3
JustinGarmon <dwightfighter@gmail.com>
2021-06-26 17:26:28 -0400

    working base code

--- mixed messages.js
+++ mixed messages.js
@@ -3,7 +3,13 @@
 
 
 
-const Mixed message=()=>{
-    const set= Math.floor(Math.random()*4)
-    const i = Math.floor(Math.random(1)*6)
+const inspirationalQuote=()=>{
+    const set= Math.floor(Math.random()*3)
+    const i = Math.floor(Math.random()*5)
+    const j = Math.floor(Math.random()*5)
+    const k = Math.floor(Math.random()*5)
+    
+    if (set = 0){console.log(`$[setA1[i]] +$[setA2[j]]+$[setA3[k]]`)}
+    else if (set = 1) {console.log (`$[setB1[i]]+$[setB2[j]]+$[setB3[k]]}}`)}
+    else if (set =2) {console.log (`$[setC1[i]]+$[setC2[j]]+$[setC3[k]]}}`)}
 }
